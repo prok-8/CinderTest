4496814729247cef3063a6f8062dcf5fe77c1421
Roki_8 <podpecanrok111@gmail.com>
2021-09-12 12:18:06 +0200

    COM Minor changes and fixes

--- src/HarmonicaSegmentGroup.h
+++ src/HarmonicaSegmentGroup.h
@@ -8,6 +8,14 @@ struct harmonica_segment_group
 {
 	std::string file_name;
 	cinder::gl::Texture2dRef texture;
-	cinder::Color color_mul;
+	cinder::ColorA color_mul;
 	bool color_mul_changed;
+
+	harmonica_segment_group(std::string file_name, cinder::gl::Texture2dRef texture) :
+		file_name(file_name),
+		texture(texture),
+		color_mul(cinder::ColorA::white()),
+		color_mul_changed(false)
+	{
+	}
 };
--- src/LearningProjApp.cpp
+++ src/LearningProjApp.cpp
@@ -25,8 +25,10 @@ using namespace ci;
 using namespace app;
 
 const vec2 harmonica_buffer_size = vec2(640, 480);
+const char* const project_json_name = "project.json";
+const char* const project_bkp_json_name = "project.json.bkp";
 
-enum config_load_status { SUCCESS = 0, MISSING_FILE, DESERIALIZATION_ERROR };
+enum config_load_status { SUCCESS = 0, FILE_LOAD_ERROR, DESERIALIZATION_ERROR };
 
 class learning_proj_app final : public App {
 public:
@@ -58,8 +60,8 @@ private:
 	int m_selected_shape_index_;
 
 	std::vector<harmonica_segment_group> m_harmonica_segments_;
-	bool m_harmonica_schedule_recalc;
-	bool m_harmonica_recalc_updated_colors_only;
+	bool m_harmonica_schedule_recalc_;
+	bool m_harmonica_recalc_updated_colors_only_;
 	gl::FboRef m_harmonica_fb_;
 
 	void write_shapes_json();
@@ -89,8 +91,8 @@ learning_proj_app::learning_proj_app() :
 		&m_rectangle_prop_,
 	},
 	m_selected_shape_index_(-1),
-	m_harmonica_schedule_recalc(false),
-	m_harmonica_recalc_updated_colors_only(false)
+	m_harmonica_schedule_recalc_(false),
+	m_harmonica_recalc_updated_colors_only_(false)
 {
 }
 
@@ -150,22 +152,19 @@ void learning_proj_app::keyDown(const KeyEvent event)
 		m_circles_.clear();
 		break;
 	case KeyEvent::KEY_ESCAPE:
-		if (isFullScreen())
-			setFullScreen(false);
-		else
-			quit();
+		setFullScreen(false);
 		break;
 	case KeyEvent::KEY_w:
-		m_last_circle_->location.y -= 4.0f;
+		m_last_circle_->location.y -= 16.0f;
 		break;
 	case KeyEvent::KEY_s:
-		m_last_circle_->location.y += 4.0f;
+		m_last_circle_->location.y += 16.0f;
 		break;
 	case KeyEvent::KEY_a:
-		m_last_circle_->location.x -= 4.0f;
+		m_last_circle_->location.x -= 16.0f;
 		break;
 	case KeyEvent::KEY_d:
-		m_last_circle_->location.x += 4.0f;
+		m_last_circle_->location.x += 16.0f;
 		break;
 	default:
 		break;
@@ -190,8 +189,9 @@ void learning_proj_app::setup()
 	ImGui::Initialize();
 
 	getWindow()->setUserData<window_user_data>(new window_user_data{ MAIN });
+	getWindow()->getSignalClose().connect([&]() {quit(); });
 	
-	const WindowRef new_window = createWindow(Window::Format());
+	const WindowRef new_window = createWindow(Window::Format().pos(100, 100));
 	new_window->setUserData(new window_user_data{ HARMONICA });
 
 	m_harmonica_fb_ = gl::Fbo::create(harmonica_buffer_size.x, harmonica_buffer_size.y);
@@ -226,8 +226,8 @@ void learning_proj_app::draw_shape_property_menus()
 			{
 				switch (read_shapes_json())
 				{
-				case MISSING_FILE:
-					m_dialog_message_ = "Load failed! Missing configuration file.";
+				case FILE_LOAD_ERROR:
+					m_dialog_message_ = "Load failed! Failed to load project file.";
 					break;
 				case DESERIALIZATION_ERROR:
 					m_dialog_message_ = "Load failed! Json parsing error.";
@@ -240,7 +240,11 @@ void learning_proj_app::draw_shape_property_menus()
 		ImGui::EndMainMenuBar();
 	}
 
-	if (ImGui::Begin("List"))	{
+	if (ImGui::Begin("Shapes")) {
+		if (ImGui::IsWindowFocused() && ImGui::IsKeyDown(KeyEvent::KEY_ESCAPE)) {
+			m_selected_shape_index_ = -1;
+		}
+		
 		ImGui::ListBox(
 			"",
 			&m_selected_shape_index_,
@@ -255,7 +259,7 @@ void learning_proj_app::draw_shape_property_menus()
 	
 	ImGui::End();
 
-	if (ImGui::Begin("Properties"))	{
+	if (ImGui::Begin("Properties", nullptr))	{		
 		if (m_selected_shape_index_ >= 0) {
 			m_property_groups_[m_selected_shape_index_]->draw();
 		}
@@ -288,10 +292,10 @@ void learning_proj_app::draw_harmonica_menus()
 				ImGui::TableNextColumn();
 				ImGui::Image(m_harmonica_segments_[i].texture, vec2(64, 64));
 				ImGui::TableNextColumn();
-				if (ImGui::ColorPicker3(
+				if (ImGui::ColorPicker4(
 					"MulColor" + i,
 					&m_harmonica_segments_[i].color_mul,
-					ImGuiColorEditFlags_NoSidePreview | ImGuiColorEditFlags_NoSmallPreview | ImGuiColorEditFlags_NoInputs | ImGuiColorEditFlags_NoLabel))
+					ImGuiColorEditFlags_NoSidePreview | ImGuiColorEditFlags_NoSmallPreview | ImGuiColorEditFlags_NoInputs | ImGuiColorEditFlags_NoLabel | ImGuiColorEditFlags_NoAlpha))
 				{
 					m_harmonica_segments_[i].color_mul_changed = true;
 					color_muls_changed = true;
@@ -305,12 +309,12 @@ void learning_proj_app::draw_harmonica_menus()
 				// Recalculating harmonica fbo here doesn't affect the final rendered image. I don't understand why.
 				schedule_harmonica_recalc(true);
 		}
-
-		ImGui::End();
 	}
+
+	ImGui::End();
 }
 
-void learning_proj_app::fileDrop(FileDropEvent event)
+void learning_proj_app::fileDrop(const FileDropEvent event)
 {
 	switch(getWindow()->getUserData<window_user_data>()->kind)
 	{
@@ -318,25 +322,24 @@ void learning_proj_app::fileDrop(FileDropEvent event)
 		break;
 	case HARMONICA:
 		const std::string file_name = event.getFile(0).string();
-		m_harmonica_segments_.push_back(harmonica_segment_group{ file_name.c_str(), gl::Texture::create(loadImage(file_name)), Color::white(), false });
+		m_harmonica_segments_.push_back(harmonica_segment_group(file_name.c_str(), gl::Texture::create(loadImage(file_name))));
 		recalc_harmonica(false);
 		break;
 	}
 }
 
 void learning_proj_app::write_shapes_json()
-{
-	const char* file_name = "output.json";
-	const char* bkp_file_name = "output.json.bkp";
-	
-	if (std::filesystem::exists(file_name))
+{	
+	if (std::filesystem::exists(project_json_name))
 	{
-		if(std::filesystem::exists(bkp_file_name))
-			remove(bkp_file_name);
-		rename(file_name, bkp_file_name);
+		if(std::filesystem::exists(project_bkp_json_name))
+			remove(project_bkp_json_name);
+		rename(project_json_name, project_bkp_json_name);
 	}
 
-	FILE* file = fopen(file_name, "wb");
+	FILE* file;
+	fopen_s(&file, project_json_name, "wb");
+	
 	char buffer[1024];
 	FileWriteStream stream(file, buffer, sizeof(buffer));
 	PrettyWriter<FileWriteStream> writer(stream);
@@ -349,17 +352,17 @@ void learning_proj_app::write_shapes_json()
 	writer.String("rectangle");
 	serialize<rectangle>(m_rectangle_, writer);
 	writer.EndObject();
-	
+
 	fclose(file);
 }
 
 config_load_status learning_proj_app::read_shapes_json()
 {
-	const char* file_name = "output.json";
+	const char* file_name = project_json_name;
 
 	FILE* file;
-	if (!(file = fopen(file_name, "rb"))) {
-		return MISSING_FILE;
+	if (fopen_s(&file, file_name, "rb")) {
+		return FILE_LOAD_ERROR;
 	}
 
 	circle circle_bkp = m_circle_;
@@ -391,7 +394,6 @@ config_load_status learning_proj_app::read_shapes_json()
 
 void learning_proj_app::draw_main()
 {
-	gl::clear(Color::gray(0.1f));
 	gl::clear(Color::gray(0.1f));
 	
 	for (moving_circle& c : m_circles_) {
@@ -429,9 +431,9 @@ void learning_proj_app::draw_main()
 
 void learning_proj_app::draw_harmonica()
 {
-	if (m_harmonica_schedule_recalc) {
-		recalc_harmonica(m_harmonica_recalc_updated_colors_only);
-		m_harmonica_schedule_recalc = false;
+	if (m_harmonica_schedule_recalc_) {
+		recalc_harmonica(m_harmonica_recalc_updated_colors_only_);
+		m_harmonica_schedule_recalc_ = false;
 	}
 	
 	if (m_harmonica_fb_) {
@@ -440,7 +442,7 @@ void learning_proj_app::draw_harmonica()
 	}
 }
 
-void learning_proj_app::recalc_harmonica(bool update_colors_only)
+void learning_proj_app::recalc_harmonica(const bool update_colors_only)
 {
 	m_harmonica_fb_->bindFramebuffer();
 	const vec2 window_size = getWindowSize();
@@ -459,12 +461,15 @@ void learning_proj_app::recalc_harmonica(bool update_colors_only)
 		m_shader_color_mul_->uniform("uWindowSize", vec2(getWindowSize()));
 
 		const float segment_width = harmonica_buffer_size.x / m_harmonica_segments_.size();
+
+		// I decided not to use a shader to combine images, but to draw them side by side separately.
+		// This also enables to update a single segment, without redrawing the entire buffer.
 		for (int i = 0; i < m_harmonica_segments_.size(); i++) {
 			if (update_colors_only && !m_harmonica_segments_[i].color_mul_changed)
 				continue;
 
 			Rectf segment_rect(segment_width * i, window_size.y - harmonica_buffer_size.y, segment_width * (i + 1), window_size.y);
-			m_shader_color_mul_->uniform("colorMul", ColorA(m_harmonica_segments_[i].color_mul, 1.0f));
+			m_shader_color_mul_->uniform("colorMul", m_harmonica_segments_[i].color_mul);
 			m_harmonica_segments_[i].texture->bind(0);
 			gl::drawSolidRect(segment_rect);
 			m_harmonica_segments_[i].color_mul_changed = false;
@@ -474,12 +479,11 @@ void learning_proj_app::recalc_harmonica(bool update_colors_only)
 	m_harmonica_fb_->unbindFramebuffer();
 }
 
-void learning_proj_app::schedule_harmonica_recalc(bool update_colors_only)
+void learning_proj_app::schedule_harmonica_recalc(const bool update_colors_only)
 {
-	m_harmonica_schedule_recalc = true;
-	m_harmonica_recalc_updated_colors_only = update_colors_only;
+	m_harmonica_schedule_recalc_ = true;
+	m_harmonica_recalc_updated_colors_only_ = update_colors_only;
 }
-
 
 // This line tells Cinder to actually create and run the application.
 CINDER_APP(learning_proj_app, RendererGl, prepare_settings)
